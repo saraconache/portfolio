1498a09a63bc230d1953d91e32916774a390e55e
Sarah Gomez <sarahvgomez@gmail.com>
2019-02-13 12:18:25 -0700

    Adding new mouseover

--- works.php
+++ works.php
@@ -23,18 +23,18 @@
 </header>
             <main id="works">
                     <section id="groupOne" class="patternA">
-                      <div class="project workA1"><a href="peach.php"><img src="images/projects/previews/peach-fr.jpg" alt="PeachnTeal Project Animated Preview"></a></div>
-                      <div class="project workA2"><a href="winelab.php"><img src="images/projects/previews/wine-fr.jpg" alt="Wine Lab Project Animated Preview"></a></div>
-                      <div class="project workA3"><a href="vizualizr.php"><img src="images/projects/previews/vizualizr-fr.jpg" alt="Vizualizr Project Animated Preview"></a></div>
-                      <div class="project workA4"><a href="tinilista.php"><img src="images/projects/previews/tinilista-fr.png" alt="tiniLista Project Animated Preview"></a></div>
-                      <div class="project workA5"><a href="mynte.php"><img src="images/projects/previews/mynte-fr.png" alt="Mynte Project Animated Preview"></a></div>
+                      <div class="project workA1 peach"><a href="peach.php"><img src="images/projects/previews/peach-fr.jpg" alt="PeachnTeal Project Animated Preview"></a></div>
+                      <div class="project workA2 winelab"><a href="winelab.php"><img src="images/projects/previews/wine-fr.jpg" alt="Wine Lab Project Animated Preview"></a></div>
+                      <div class="project workA3 vizualizr"><a href="vizualizr.php"><img src="images/projects/previews/vizualizr-fr.jpg" alt="Vizualizr Project Animated Preview"></a></div>
+                      <div class="project workA4 tinilista"><a href="tinilista.php"><img src="images/projects/previews/tinilista-fr.png" alt="tiniLista Project Animated Preview"></a></div>
+                      <div class="project workA5 mynte"><a href="mynte.php"><img src="images/projects/previews/mynte-fr.png" alt="Mynte Project Animated Preview"></a></div>
                     </section>
                     
                     <section id="groupTwo" class="patternB">
-                        <div class="project workB1"><a href="afcc.php"><img src="images/projects/previews/afcc-fr.png" alt="AFCC Project Animated Preview"></a></div>
-                        <div class="project workB2"><a href="dkki.php"><img src="images/projects/previews/dkki-fr.jpg" alt="DKKI Project Animated Preview"></a></div>
-                        <div class="project workB3"><a href="bigworld.php"><img src="images/projects/previews/bigworld-fr.png" alt="Big World Project Animated Preview"></a></div>
-                        <div class="project workB4"><a href="jethydrovac.php"><img src="images/projects/previews/hydrovac-fr.jpg" alt="Jet Hydrovac Project Animated Preview"></a></div>
+                        <div class="project workB1 afcc"><a href="afcc.php"><img src="images/projects/previews/afcc-fr.png" alt="AFCC Project Animated Preview"></a></div>
+                        <div class="project workB2 dkki"><a href="dkki.php"><img src="images/projects/previews/dkki-fr.jpg" alt="DKKI Project Animated Preview"></a></div>
+                        <div class="project workB3 bigworld"><a href="bigworld.php"><img src="images/projects/previews/bigworld-fr.png" alt="Big World Project Animated Preview"></a></div>
+                        <div class="project workB4 hydrovac"><a href="jethydrovac.php"><img src="images/projects/previews/hydrovac-fr.jpg" alt="Jet Hydrovac Project Animated Preview"></a></div>
                     </section>
             </main>
 <script async defer src="js/main.js"></script> 
